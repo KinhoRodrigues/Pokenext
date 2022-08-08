267b307c1a57b37b31096fd0cacfb1d189ec577c
KinhoRodrigues <91065012+KinhoRodrigues@users.noreply.github.com>
2022-08-08 10:26:19 -0300

    Ajustes

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function About() {
   return (
     <div className={styles.about}>
-      <h1>Sobre o projeto</h1>
+      <h1>About projects</h1>
       <p>PokeNext é um App construído em Next.js para consultar Pokémons.</p>
       <Image
         src="/images/charizard.png"
